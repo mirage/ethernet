d3e98f700d59ca95a2e13b66665b4d4ad8aed8b6
Hannes Mehnert <hannes@mehnert.org>
2018-11-27 00:41:46 +0100

    adjust to mirage-net 2.0.0 and mirage-protocols 2.0.0

--- src/ethernet.ml
+++ src/ethernet.ml
@@ -20,8 +20,6 @@ open Lwt.Infix
 let src = Logs.Src.create "ethernet" ~doc:"Mirage Ethernet"
 module Log = (val Logs.src_log src : Logs.LOG)
 
-let default_mtu = 1500
-
 module Make(Netif : Mirage_net_lwt.S) = struct
 
   type 'a io = 'a Lwt.t
@@ -33,11 +31,10 @@ module Make(Netif : Mirage_net_lwt.S) = struct
 
   type t = {
     netif: Netif.t;
-    mtu: int;
   }
 
   let mac t = Netif.mac t.netif
-  let mtu t = t.mtu
+  let mtu t = Netif.mtu t.netif (* interface MTU excludes Ethernet header *)
 
   let input ~arpv4 ~ipv4 ~ipv6 t frame =
     let open Ethernet_packet in
@@ -48,36 +45,40 @@ module Make(Netif : Mirage_net_lwt.S) = struct
     match Unmarshal.of_cstruct frame with
     | Ok (header, payload) when of_interest header.destination ->
       begin
-        let open Ethernet_wire in
-        match header.ethertype with
-        | ARP -> arpv4 payload
-        | IPv4 -> ipv4 payload
-        | IPv6 -> ipv6 payload
+        match header.Ethernet_packet.ethertype with
+        | `ARP -> arpv4 payload
+        | `IPv4 -> ipv4 payload
+        | `IPv6 -> ipv6 payload
       end
     | Ok _ -> Lwt.return_unit
     | Error s ->
-      Log.debug (fun f -> f "Dropping Ethernet frame: %s" s);
+      Log.debug (fun f -> f "dropping Ethernet frame: %s" s);
       Lwt.return_unit
 
-  let write t frame =
+  let write t ?src destination ethertype ?size payload =
     MProf.Trace.label "ethernet.write";
-    Netif.write t.netif frame >|= function
+    let source = match src with None -> mac t | Some x -> x in
+    let fill frame =
+      match
+        Ethernet_packet.Marshal.into_cstruct
+          { source ; destination ; ethertype }
+          frame
+      with
+      | Error msg ->
+        Log.err (fun m -> m "error %s while marshalling ethernet header into allocated buffer" msg);
+        0
+      | Ok () ->
+        payload (Cstruct.shift frame Ethernet_wire.sizeof_ethernet)
+    in
+    Netif.write t.netif ?size fill >|= function
     | Ok () -> Ok ()
     | Error e ->
       Log.warn (fun f -> f "netif write errored %a" Netif.pp_error e) ;
       Error e
 
-  let writev t bufs =
-    MProf.Trace.label "ethernet.writev";
-    Netif.writev t.netif bufs >|= function
-    | Ok () -> Ok ()
-    | Error e ->
-      Log.warn (fun f -> f "netif writev errored %a" Netif.pp_error e) ;
-      Error e
-
-  let connect ?(mtu = default_mtu) netif =
+  let connect netif =
     MProf.Trace.label "ethernet.connect";
-    let t = { netif; mtu } in
+    let t = { netif } in
     Log.info (fun f -> f "Connected Ethernet interface %a" Macaddr.pp (mac t));
     Lwt.return t
 
--- src/ethernet_wire.ml
+++ src/ethernet_wire.ml
@@ -6,10 +6,13 @@ type ethernet = {
   } [@@big_endian]
 ]
 
-[%%cenum
-type ethertype =
-  | ARP  [@id 0x0806]
-  | IPv4 [@id 0x0800]
-  | IPv6 [@id 0x86dd]
-  [@@uint16_t]
-]
+let ethertype_to_int = function
+  | `ARP -> 0x0806
+  | `IPv4 -> 0x0800
+  | `IPv6 -> 0x86dd
+
+let int_to_ethertype = function
+  | 0x0806 -> Some `ARP
+  | 0x0800 -> Some `IPv4
+  | 0x86dd -> Some `IPv6
+  | _ -> None
